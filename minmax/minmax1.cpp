e16766513431dd4e26555d261da7014641fdf3f6
nbaglietto <37049113+nbaglietto@users.noreply.github.com>
2020-09-17 21:29:16 -0300

    Update minmax1.cpp

--- minmax/minmax1.cpp
+++ minmax/minmax1.cpp
@@ -3,44 +3,43 @@
 #include<stdlib.h>
 #include<string.h>
 
+
 int main() {
-	float i;
-	int temperatura;
-	int tmax=0;
-	int tmin=0;
-	int cont=0;
-	int num=0;
-	
+			int num=0,cont=0,tmin=0,tmax=0,temperatura=0;
+
 	printf("Cuantas mediciones de Temperatura ingresara?: \n");
 	scanf("%d",&num);
 	
-
 		if (cont!=num);
 		{	
 				for(cont=0;cont<num;cont++)  
 		   		{
 					printf("Ingrese la Temperatura ");
 					scanf("%d",&temperatura);
-		
-
+					
+					if (cont==1)	
+						{
+						tmax=temperatura;
+						tmin=temperatura;
+						}
+					if (temperatura < tmin)
+						{
+						tmin=temperatura;	
+						}
+					else {
+					
 					if (temperatura > tmax)
 						{
 						tmax = temperatura;
 						}
-					else 
-						{
-						if(temperatura< tmin)
-						tmin = temperatura;							
-						}
-		
+					}
 				}//fin for
 		}//fin if contador
-	
-	
+
 	printf("\n\n\n");	
 	printf(" La tempera maxima es de: %d \n ",tmax ); 
 	printf("La tempera minima es de: %d \n ",tmin );
 	printf("\n\n\n");
-
+		
 system("pause");
 }
